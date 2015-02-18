a57a4ab7eacc279f4a170fe0a0c71ae2b6623244
Dr Romain Caze <rcaze@imperial.ac.uk>
2015-02-18 17:33:10 +0000

    Add the motive_curve function

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-"""Project on the collaboration with Alex"""
+"""Collaborative project with Alex"""
 import numpy as np
 import pandas as pd #Panda library used to handle Alex Data
 import os
@@ -9,9 +9,6 @@ import unittest
 from traits.api import HasTraits, Float, Function, Array, List, Int, Dict
 
 project_name = "Berdi2014"
-
-def foo():
-    pass
 
 def import_data_berdi():
     path = "~/Data/" + project_name + "/"
@@ -24,7 +21,7 @@ def stimulus(duration, probability):
     """Generate a stimulus of length duration"""
     return np.random.binomial(1, probability, (duration,))
 
-class TestGeneratingStimulus(unittest.TestCase):
+class TestStimulus(unittest.TestCase):
     """Testing the generation of stimulus"""
     def setUp(self):
         self.duration = 100000
@@ -51,88 +48,21 @@ def analysis(L, G, n_chunks=10):
         spe[i], sen[i] = spe_sen(G_split[i], L_split[i])
     return spe, sen
 
-def motivation_effect(q, motivation):
+def motive_curve(init_val, n_trials):
+    """Define the motivation curve as a linear curve with 0 in the middle"""
+    slope = init_val/float((n_trials-1)/2.)
+    return [init_val - i*slope for i in range(n_trials)]
+
+def set_qnext_motiv(q, motivation):
     """Modify the Q-values depending on the motivation of the agent"""
     q_motiv = np.zeros(q.shape)
     for action in range(2):
         for state in range(2):
             if action == 1:
-                q_motiv[state, action] = q[state, action] + 3*motivation
+                q_motiv[state, action] = q[state, action] + motivation
             else:
-                q_motiv[state, action] = q[state, action] - 3*motivation
+                q_motiv[state, action] = q[state, action] - motivation
     return q_motiv
-
-def testbed(states, q_init = np.zeros((2,2)), learning=False, motivation=True, thirst=1):
-    """
-    Launch a testbed determined by the obs array for an agent with one or two fix or plastic learning rates
-
-    PARAMETERS
-    -----
-    states: array
-        observation correponding to states
-
-    RETURNS
-    -------
-    rec_choice: array
-         recording the choice of the agent for each episode and each iteration
-    rec_q: array
-         recording the internal q-values of the agent
-    rec_reward: array
-         recording the learning rates
-    """
-    n_trials = len(states)
-
-    rec_q = np.zeros((n_trials, 2, 2), np.float)
-    rec_action = np.zeros(n_trials, np.int)
-    rec_reward = np.zeros(n_trials, np.int)
-
-    q_est = q_init
-    for i in range(n_trials):
-        if motivation:
-            #Modify the Q estimates given the motivation
-            q_est = motivation_effect(q_init, thirst)
-        #Choose given the Q estimates and the state
-        action = softmax(q_est[states[i]])
-        #Record the choice
-        rec_action[i] = action
-        if states[i] == 1:
-            if action == 1:
-                reward = 1
-            else:
-                reward = 0
-        else:
-            reward = 0
-        rec_reward[i] = reward
-        #Update the q_values given the state, action and reward
-        if learning:
-            q_est[states[i], action] = set_qnext(q_est[states[i], action], reward)
-
-        #Record Q estimate
-        rec_q[i] = q_est
-        thirst -= 0.01
-        #import pdb; pdb.set_trace()
-
-    return rec_q, rec_action, rec_reward
-
-def motiv_expe(itsit, motivation):
-    """An experiment where the motivation matters
-    #Define the intensity of licking bla
-    itsit = 1"""
-    #For action 0 (no-go) then there is no-lick (0)
-    #And for action
-    if motivation:
-        #Initiate the motivation
-        q_init = np.array([[itsit,-itsit],
-                           [-itsit,itsit]])
-    else:
-        q_init = np.array([[itsit,itsit],
-                           [itsit,itsit]])
-    thirst = itsit
-
-    states = stimulus(200, 0.5)
-    rec_q, rec_action, rec_reward = testbed(states, q_init, learning=True, motivation=motiv)
-
-    return states, rec_action
 
 def spe_sen(target, actual):
     """Compute the (specificity,sensitivity) couple and the Matthews correlation coefficient
@@ -208,62 +138,57 @@ def softmax(qval=[0.5,0.9], temp=1):
             return choice
         choice += 1
 
-#Working but not used only when required KIS
-class Agent(HasTraits):
-    """Define an reinforcement learning agent"""
-    alpha = Float(0.1)
-    decision_function = Function(softmax())
-    q_values = Array(dtype=np.float, value=np.zeros((2,2)))
-    q_rec = List()
-    temperature = Float(0.3)
+def testbed(states, q_init = np.zeros((2,2)), learning=True, motivation=True, thirst=1):
+    """
+    Launch a testbed determined by the obs array for an agent with one or two fix or plastic learning rates
 
-    def choose(self, state):
-        """Produce an action given the q-values and the state"""
-        return self.decision_function(self.q_values[state], self.temperature)
+    PARAMETERS
+    -----
+    states: array
+        observation correponding to states
 
-    def update(self, state, action, reward):
-        """Update its q-values given the reward and the state"""
-        qnext = set_qnext(self.q_values[state, action], reward)
-        self.q_values[state, action] = qnext
-        self.q_rec.append(qnext)
+    RETURNS
+    -------
+    rec_choice: array
+         recording the choice of the agent for each episode and each iteration
+    rec_q: array
+         recording the internal q-values of the agent
+    rec_reward: array
+         recording the learning rates
+    """
+    n_trials = len(states)
+    thirst = motive_curve(3, n_trials)
 
-#Not used now, only when required KIS principle
-class Task(HasTraits):
-    """Define a task"""
-    duration = Int(100)
-    states = Array(value=np.random.binomial(1,0.5,100))
-    actions = Dict({0:0, 1:1})
-    expected_action = Array()
-    reward = Array()
-    agents = List(Agent)
+    rec_q = np.zeros((n_trials, 2, 2), np.float)
+    rec_action = np.zeros(n_trials, np.int)
+    rec_reward = np.zeros(n_trials, np.int)
 
-    def __init__(self):
-        #Setting the reward like in Alex's task
-        self.expected_action = self.states
-        if len(self.agents) >= 1:
-            self.actions = np.zeros((self.duration, len(self.agents)))
-        else:
-            self.actions = np.zeros(self.duration)
-
-    def one_step(self, state):
-        """compute one simulation step"""
-        action = self.agents[0].choose(state)
-        if action == self.expected_action(state):
-            reward = 1
-        else:
-            reward = -1
-        self.agents[0].update(state, action, reward)
-
-def motivation_effect(q, motivation):
-    """Modify the Q-values depending on the motivation of the agent"""
-    q_motiv = np.zeros(q.shape)
-    for action in range(2):
-        for state in range(2):
+    q_est = q_init
+    for i in range(n_trials):
+        if motivation:
+            #Modify the Q estimates given the motivation
+            q_est = set_qnext_motiv(q_init, thirst[i])
+        #Choose given the Q estimates and the state
+        action = softmax(q_est[states[i]])
+        #Record the choice
+        rec_action[i] = action
+        if states[i] == 1:
             if action == 1:
-                q_motiv[state, action] = q[state, action] + 3*motivation
+                reward = 1
             else:
-                q_motiv[state, action] = q[state, action] - 3*motivation
-    return q_motiv
+                reward = 0
+        else:
+            reward = 0
+        rec_reward[i] = reward
+        #Update the q_values given the state, action and reward
+        if learning:
+            q_est[states[i], action] = set_qnext(q_est[states[i], action], reward)
+
+        #Record Q estimate
+        rec_q[i] = q_est
+        #import pdb; pdb.set_trace()
+
+    return rec_q, rec_action, rec_reward
 
 class TestTestbed(unittest.TestCase):
     """Testing the testbed"""
@@ -273,6 +198,27 @@ class TestTestbed(unittest.TestCase):
         obs = np.array([stimulus(prob, duration),
                         stimulus(prob, duration)])
         rec_q, rec_choice, rec_rewad = testbed(obs)
+
+def motiv_expe(itsit, motivation, learning):
+    """An experiment where the motivation matters
+    #Define the intensity of licking bla
+    itsit = 1"""
+    #For action 0 (no-go) then there is no-lick (0)
+    #And for action
+    if motivation:
+        #Initiate the motivation
+        q_init = np.array([[itsit,-itsit],
+                           [-itsit,itsit]])
+    else:
+        q_init = np.array([[itsit,itsit],
+                           [itsit,itsit]])
+    thirst = itsit
+
+    states = stimulus(200, 0.5)
+    rec_q, rec_action, rec_reward = testbed(states, q_init, learning=learning, motivation=motivation)
+
+    return states, rec_action
+
 
 def sliding_estimate(target, actual, window_s=20):
     """Compute the specificity and sensitivity given a sliding window"""
@@ -293,9 +239,13 @@ if __name__=="__main__":
     spe = np.zeros((repetition, n_c))
     sen = np.zeros((repetition, n_c))
     itsit = 1
-    motiv = False
+    motiv = True
+    learning = False
     for i in range(repetition):
-        G, L = motiv_expe(itsit, motiv)
+        G, L = motiv_expe(itsit, motiv, learning)
         spe[i], sen[i] = analysis(L, G, n_c)
-    plt.plot(1 - np.mean(spe, axis=0), color='g')
+    plt.plot(1 - np.mean(spe, axis=0), color='r')
     plt.plot(np.mean(sen, axis=0), color='g')
+    plt.show()
+    print motive_curve(3, 8)
+
