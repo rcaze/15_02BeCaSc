91728baeae1ecfea2ff5c74aa76595cec22bbdf3
Dr Romain Caze <rcaze@imperial.ac.uk>
2015-04-09 13:58:14 +0100

    Start the development of the package

---
+++ motivBCS2015/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='motivBCS2015',
+      version='0.1',
+      description='Code used to generate Figure.3 of our article',
+      url='http://github.com/rcaze/motivBCS2015',
+      author='Romain Caze',
+      author_email='r.caze@iimperial.ac.uk',
+      license='GNU',
+      packages=['motivBCS2015'],
+      zip_safe=False)
